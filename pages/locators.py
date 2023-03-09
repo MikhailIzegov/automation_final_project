04750bf0cee84a753ced47ae99a7505d0ac9e450
MikhailIzegov <mikhail.izegov@yandex.ru>
2023-03-09 15:42:39 +0300

    more tests are added including negative ones (+ login test is transferred to base_page)

--- pages/locators.py
+++ pages/locators.py
@@ -1,8 +1,13 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators():
+class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")  # for checking the correct error message
+
+
+class MainPageLocators():
+    pass
 
 
 class LoginPageLocators():
@@ -26,5 +31,6 @@ class ProductPageLocators():
     AVAILABILITY = (By.CSS_SELECTOR, ".product_main > .availability")
     WRITE_REVIEW = (By.CSS_SELECTOR, "#write_review")
     PRODUCT_INFO_TABLE = (By.CSS_SELECTOR, ".table-striped")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages > .alert-success:nth-child(1)")
     NAME_OF_ADDED_PRODUCT = (By.CSS_SELECTOR, "div.alert:nth-child(1) strong")
     TOTAL_PRICE = (By.CSS_SELECTOR, ".alertinner p strong")
--- pages/product_page.py
+++ pages/product_page.py
@@ -4,10 +4,9 @@ from .locators import ProductPageLocators
 
 class ProductPage(BasePage):
     def should_be_product_page(self):
-        self.should_be_product_page_url()
         self.should_be_product_page_attributes()
 
-    def should_be_product_page_url(self):
+    def should_be_product_page_promo_offer_url(self):
         assert "?promo=offer" in self.browser.current_url, "Seems like it's NOT target product page: check URL"
 
     def should_be_product_page_attributes(self):
@@ -36,6 +35,14 @@ class ProductPage(BasePage):
         total_basket_price = self.browser.find_element(*ProductPageLocators.TOTAL_PRICE).text
         assert product_price == total_basket_price, "Product price and total basket price are not equal (mb because " \
                                                     "of more than 1 product in the basket!"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_disappear_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is not disappeared"
 
 
 
